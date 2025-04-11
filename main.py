ef9d764c259593df5858f5c4cca4504e334e703d
Станислав Рубцов <frexrub@gmail.com>
2025-04-11 15:01:33 +0300

    add main and logs

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def main():
+#    ['main.py', 'logs/app1.log', 'logs/app2.log', 'logs/app3.log', '--report', 'handlers']
+
+    print(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
